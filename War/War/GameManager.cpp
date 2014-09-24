9609efbdbcf16e4af3efb0576353164bf75b880c
jackofalltrades <hayesnj1@gcc.edu>
2014-09-23 20:40:39 -0400

    Updated deck.cpp, added GameManager functions, updated GameManager.h

--- War/War/GameManager.cpp
+++ War/War/GameManager.cpp
@@ -1,1 +1,28 @@
 #include "GameManager.h"
+#include "Card.h"
+#include "Player.h"
+#include "Deck.h"
+
+bool gameManager::playerWins(Card player, Card computer)
+{
+	if (player.getValue() > computer.getValue())
+	{
+		return true;
+	}
+	return false;
+}
+
+bool gameManager::isWar(Card player, Card computer)
+{
+	return player.getValue()==computer.getValue();
+}
+
+void gameManager::war(player human, player computer)
+{
+
+}
+
+bool gameManager::gameOver(player human, player computer)
+{
+
+}
