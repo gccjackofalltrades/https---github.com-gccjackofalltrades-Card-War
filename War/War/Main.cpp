ca40b6e9e3f243be9a62fb8cee7b572bfdbf98f0
unknown <ABRAHAMJH1@2CE3281264.GCC.edu>
2014-09-24 19:57:57 -0400

    started on main

--- War/War/Main.cpp
+++ War/War/Main.cpp
@@ -12,7 +12,44 @@ using std::endl;
 
 int main()
 {
-	
+	deck theDeck;
+	player p1(26);
+	player computer(26);
+	gameManager overLord;
+	overLord.deal(theDeck, p1, computer);
+	Card playersNextCard = p1.playNextCard();
+	Card computersNextCard = computer.playNextCard();
+	while(!overLord.gameOver(p1, computer))
+	{
+
+		if(overLord.isWar(playersNextCard, computersNextCard))
+		{
+			overLord.war(p1, computer, playersNextCard, computersNextCard);
+			if (overLord.gameOver(p1, computer))
+			{
+				break;
+			}
+		}
+		else if (overLord.playerWins(playersNextCard, computersNextCard))
+		{
+			cout << "Player's ";
+			overLord.printCard(playersNextCard);
+			cout << "Beats the Computer's ";
+			overLord.printCard(computersNextCard);
+			p1.addToDiscard(playersNextCard);
+			p1.addToDiscard(computersNextCard);
+		}
+		else if(overLord.playerWins(computersNextCard, playersNextCard))
+		{
+			cout << "Computer's ";
+			overLord.printCard(playersNextCard);
+			cout << "Beats the Player's ";
+			overLord.printCard(computersNextCard);
+			computer.addToDiscard(playersNextCard);
+			computer.addToDiscard(computersNextCard);
+		}
+
+	}
 	return 0;
 }
 
