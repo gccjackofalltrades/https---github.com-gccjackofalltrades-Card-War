839fa66e6a0e4f91c7dc8b3ce7a39fad9cef9206
idbidb123 <idbidb123@users.noreply.github.com>
2014-09-23 20:47:18 -0400

    Update Player.cpp

--- War/War/Player.cpp
+++ War/War/Player.cpp
@@ -2,22 +2,51 @@
 #include "Card.h"
 #include <vector>
 
+Card player::gethandCard (int location)
+{
+	return hand[location];
+}
+
 void player::sethandCard (int location, Card c)
 {
 	hand[location] = c;
 }
+
+Card player::getDiscardCard(int location)
+{
+	return discard[location];
+}
+
 void player::setDiscardCard(int location, Card d)
 {
 	discard[location] = d;
 }
+
+int player::getHandSize()
+{
+	return handSize;
+}
+
 void player::setHandSize(int size)
 {
 	handSize = size;
 }
+
+int player::getDiscardSize()
+{
+	return discardSize;
+}
+
 void player::setDiscardSize()
 {
 	discardSize = discard.size();
 }
+
+int player::getCardTotal()
+{
+	return cardTotal;
+}
+
 void player::setCardTotal(int total)
 {
 	cardTotal = total;
