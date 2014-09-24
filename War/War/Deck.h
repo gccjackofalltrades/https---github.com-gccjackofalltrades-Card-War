98c9b6f0dce897426292fdd2f923f4f37ba3b66c
johnsonra1 <johnsonra1@gcc.edu>
2014-09-24 08:39:17 -0400

    Added Shuffle declarations
    Add declarations to deck.h and definitions to deck.cpp

--- War/War/Deck.h
+++ War/War/Deck.h
@@ -4,21 +4,24 @@
 
 #include "Card.h"
 #include <vector>
-<<<<<<< HEAD
-#include "Deck and Shuffler.cpp"
 using std::vector;
 
-=======
 using std::vector;
->>>>>>> 0ba59b10817471daa91a60775de09c99a5aedd81
 class deck
 {
 public:
 	Card getCard (int location);
 	void setCard (Card c, int location);
-	friend void shuffle_deck();
+    void create_deck(); //creates deck using value_setter and Suit_setter, store into a vector deck_of_cards
+	void shuffle_deck();//shuffles the deck through swapping index values
+	void size_the_deck();//set the deck size to 52
 private:
 	vector<Card> deck_of_cards;
 };
+
+Value value_setter(int j);//accepts index j, goes from 0 to 12, returns appropriate card value
+Suit suit_setter(int i); //accepts index i, goes from 0 to 3, returns appropriate suit value
+
+
 
 #endif
