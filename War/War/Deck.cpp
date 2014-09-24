48ea4ff407341ebbec8491f78cb70760468ed997
unknown <ABRAHAMJH1@2CE3281264.GCC.edu>
2014-09-23 21:08:38 -0400

    definitions for deck functions

---
+++ War/War/Deck.cpp
@@ -0,0 +1,12 @@
+#include "Deck.h"
+#include "Card.h"
+#include <vector>
+
+Card deck::getCard(int location)
+{
+	return deck[location];
+}
+void deck::setCard(Card c, int location)
+{
+	deck[location] = c;
+}
